93bbfd775f642a5549c6b8c963c4823e5944525b
Abhishek Man Basnet <abhishekmanbasnet@hotmail.com>
2025-12-28 12:10:09 +0545

    feat: update api client for bearer token

--- lib/core/network/api_client.dart
+++ lib/core/network/api_client.dart
@@ -1,29 +1,24 @@
 import 'package:dio/dio.dart';
-import 'dart:convert';
 
 class ApiClient {
-  static const String baseApiUrl = "http://10.0.2.2:8000/";
-  final String _username = "joshua";
-  final String _password = "1902";
-  late final Dio dio;
-  ApiClient() {
-    // 1. Create the Basic Auth string
-    String basicAuth = 'Basic ${base64Encode(utf8.encode('$_username:$_password'))}';
+  static const String baseApiUrl = "http://10.0.2.2:8000/api/v1/";
+  final Dio dio;
 
-    dio = Dio(
-      BaseOptions(
-        baseUrl: baseApiUrl,
-        connectTimeout: const Duration(seconds: 5),
-        receiveTimeout: const Duration(seconds: 3),
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'Authorization': basicAuth, // 2. Attach the credentials here
-        },
-      ),
-    );
-
+  ApiClient() : dio = Dio(
+    BaseOptions(
+      baseUrl: baseApiUrl,
+      connectTimeout: const Duration(seconds: 5),
+      receiveTimeout: const Duration(seconds: 3),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    ),
+  ) {
     dio.interceptors.add(LogInterceptor(responseBody: true, requestBody: true));
   }
 
+  void setToken(String token) {
+    dio.options.headers['Authorization'] = 'Bearer $token';
+  }
 }
